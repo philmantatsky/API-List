6bd86f9252b9a2788bd5f5953d2cdd6a58b30cf4
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-02-29 09:29:46 -0600

    Initial Commit

---
+++ API List/API_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ListApp.swift
+//  API List
+//
+//  Created by Phillip Mantatsky on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct API_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
